10a151b304b1218718bcf9b7d252ca81ddf76ec6
Jack Murphy <jmurphy.web@gmail.com>
2017-04-16 21:15:35 +0100

    add a few routes innit

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -3,6 +3,34 @@ defmodule Discuss.TopicController do
 
   alias Discuss.Topic
 
+  def edit conn, %{"id" => id} do
+    topic = Repo.get(Topic, id)
+    changeset = Topic.changeset(topic)
+
+    render conn, "edit.html", changeset: changeset, topic: topic
+  end
+
+  def update conn, %{"topic" => updated_topic, "id" => id} do
+    old_topic = Repo.get(Topic, id)
+    changeset = Topic.changeset(old_topic, updated_topic)
+
+    case Repo.update(changeset) do
+      {:ok, _topic} ->
+        conn
+        |> put_flash(:info, "Topic Updated")
+        |> redirect to: topic_path(conn, :index)
+      {:error, changeset} ->
+        render conn, "edit.html", changeset: changeset, topic: old_topic
+    end
+
+  end
+
+  def index conn, params do
+    topics = Repo.all(Topic)
+
+    render conn, "index.html", topics: topics
+  end
+
   def new conn, _params do
     changeset = Topic.changeset(%Topic{}, %{})
 
@@ -14,7 +42,10 @@ defmodule Discuss.TopicController do
     changeset = Topic.changeset(%Topic{}, topic)
 
     case Repo.insert(changeset) do
-      {:ok, post} -> IO.inspect post
+      {:ok, _topic} ->
+        conn
+        |> put_flash(:info, "Topic Created")
+        |> redirect(to: topic_path(conn, :index))
       {:error, changeset} ->
         render conn, "new.html", changeset: changeset
     end
